caf53546ece6ed87901305a94ebee0e7468f71a0
Karan Sikka <kssworld93@gmail.com>
2014-12-19 15:43:35 -0500

    fixed test

--- basic/test_sort.py
+++ basic/test_sort.py
@@ -1,24 +1,8 @@
 import unittest
 from sort import *
 
-def swap( arr, x, y ):
-  tmp = arr[x]
-  arr[x] = arr[y]
-  arr[y] = tmp
-
-def bubblesort(arr):
-    for i in range( len( arr ) ):
-      for k in range( len( arr ) -1):
-        if ( arr[k] > arr[k + 1] ):
-          swap( arr, k, k + 1 )
-
-    print(arr)  
-    return arr
-
 inputs = [ '9 1 6 4 5 10 8 7 2 3']
 inputs = [ [int(i) for i in inp.split()] for inp in inputs ]
-
-print(inputs)
 
 class SortTest(unittest.TestCase):
 
@@ -26,6 +10,17 @@ class SortTest(unittest.TestCase):
         for inp in inputs:
             out = bubblesort(inp)
             self.assertListEqual( out, list(sorted(inp)) )
+
+    def test_selectionsort(self):
+        for inp in inputs:
+            out = selectionsort(inp)
+            self.assertListEqual( out, list(sorted(inp)) )
+
+    def test_mergesort(self):
+        for inp in inputs:
+            out = mergesort(inp)
+            self.assertListEqual( out, list(sorted(inp)) )
+
 
 
 if __name__ == "__main__":
