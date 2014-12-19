43db9ee96bdd02827d1e8986a9dcadf23125d717
ssikka100 <sonusikka@gmail.com>
2014-12-19 15:33:06 -0500

    last

--- basic/test_sort.py
+++ basic/test_sort.py
@@ -1,10 +1,23 @@
 import unittest
 from sort import *
 
+def swap( arr, x, y ):
+  tmp = arr[x]
+  arr[x] = arr[y]
+  arr[y] = tmp
+
+def bubblesort(arr):
+    for i in range( len( arr ) ):
+      for k in range( len( arr ) -1):
+        if ( arr[k] > arr[k + 1] ):
+          swap( arr, k, k + 1 )
+     
+    return arr
 
 inputs = [ '9 1 6 4 5 10 8 7 2 3']
 inputs = [ [int(i) for i in inp.split()] for inp in inputs ]
 
+print(inputs)
 
 class SortTest(unittest.TestCase):
 
@@ -12,7 +25,7 @@ class SortTest(unittest.TestCase):
         for inp in inputs:
             out = bubblesort(inp)
             self.assertListEqual( out, list(sorted(inp)) )
-
+print(inputs)
 
 if __name__ == "__main__":
     unittest.main()
