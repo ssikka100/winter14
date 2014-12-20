c6c2e44db688fa0bac786a130d5cec1ac9380e8c
ssikka <sonusikka@gmail.com>
2014-12-20 02:35:53 -0500

    workingall

--- basic/sort.py
+++ basic/sort.py
@@ -1,4 +1,3 @@
-
 def swap( arr, x, y ):
   tmp = arr[x]
   arr[x] = arr[y]
@@ -13,20 +12,18 @@ def bubblesort(arr):
           swap( arr, k, k + 1 )
     return arr
 
-
 def selectionsort(arr):
-    for i in range(len(arr)-1):
+    for i in range(len(arr)):
         currMin=i
-        for k in range (i+1, len(arr)-1,1):
+        for k in range (i+1, len(arr),1):
             if(arr[k] < arr[currMin]):
                currMin=k
                
         if(currMin != i):
-           swap(arr, arr[i], arr[currMin])
+           swap(arr, i, currMin)
            
     return arr
         
-
 def mergesort(arr):
     if len(arr) < 2:
       return arr
@@ -40,7 +37,6 @@ def mergesort(arr):
     
     return(merge(left,right))
     
-
 def merge(left, right):
 
     sorted = []
@@ -61,19 +57,17 @@ def merge(left, right):
 
     return sorted
   
-
 def quicksort(arr):
   if (len(arr) < 2):
     return arr
   else:
     return(quick(arr,0,len(arr)-1))
-  
-    
+      
 def partition(arr, start,end):
   i = start-1
   pivot = arr[end]
 
-  for j in range(start,end-1):
+  for j in range(start,end):
     if(arr[j] < pivot):
       i+=1
       swap(arr,i,j)
